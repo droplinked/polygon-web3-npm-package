7f6dbe7d0fbde37715afc3be200d25606546a3da
Mohammad matin <moh.gh@aut.ac.ir>
2023-07-06 21:30:43 +0330

    edited : gating process

--- gating.ts
+++ gating.ts
@@ -1,29 +1,17 @@
-import * as Moralisd from 'moralis'
-let Moralis = Moralisd.default;
-let isMoralisInitialized = false;
-export async function getAccountBalance(NFTContractAddress: string, address: any, network : "polygon-testnet" | "polygon-mainnet" | "binance-testnet" | "binance-mainnet"): Promise<number>{
-    let chain : any = null;
-    switch (network){
-        case "polygon-testnet":
-            chain = Moralis.EvmUtils.EvmChain.MUMBAI;
-            break;
-        case "polygon-mainnet":
-            chain = Moralis.EvmUtils.EvmChain.POLYGON;
-            break;
-        case "binance-testnet":
-            chain = Moralis.EvmUtils.EvmChain.BSC_TESTNET;
-            break;
-        case "binance-mainnet":
-            chain = Moralis.EvmUtils.EvmChain.BSC;
-        default:
-            throw new Error("Invalid network");
-    }
-
-    const response = await Moralis.EvmApi.nft.getWalletNFTs({
-        "address" : address,
-        "chain" : chain,
-    });
-    return (response as any).toJSON().result.filter((value: { token_address: string; }) => value.token_address.toLowerCase() == NFTContractAddress.toLowerCase()).length;
+import {ethers} from 'ethers'
+let erc721ABI = [{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}];
+/**
+ * 
+ * @param {string} NFTContractAddress 
+ * @param {string} address 
+ * @returns 
+ */
+export async function getAccountBalance(NFTContractAddress: string, address: string, network : "mainnet" | "testnet"){
+    let jsonRpcProvider = network == "mainnet" ? "https://polygon-mainnet.infura.io" : "https://rpc-mumbai.maticvigil.com";
+    let provider = new ethers.JsonRpcProvider(jsonRpcProvider);
+    let contract = new ethers.Contract(NFTContractAddress, erc721ABI, provider);
+    let balance = await contract.balanceOf(address);
+    return Number(balance.toString());
 }
 
 /**
@@ -32,27 +20,27 @@ export async function getAccountBalance(NFTContractAddress: string, address: any
  * @param {string} ownerAddress 
  * @returns 
  */
-async function get_address_amount(contract_address: string, ownerAddress: any, network : "polygon-testnet" | "polygon-mainnet" | "binance-testnet" | "binance-mainnet"): Promise<number>{
+async function get_address_amount(contract_address: string, ownerAddress: string, network : "mainnet" | "testnet"){
     return Number(await getAccountBalance(contract_address, ownerAddress, network));
 }
 
 /**
  * @param {string} address 
- * @param {{addresses : [string], discountPercentage: number, nftsCount : number , description:string, _id : string}} rule 
+ * @param {{addresses : [string], discountPercentage: number, nftsCount : number}} rule 
  * @param {*} reedemedNFTs 
  * @returns 
  */
 async function nonGatedPassesRules(
-    address: any,
-    rule: { addresses: string | any[]; nftsCount: number; },
-    reedemedNFTs: string | any[],
-    network : "polygon-testnet" | "polygon-mainnet" | "binance-testnet" | "binance-mainnet"
-): Promise<{ passes: boolean; NFTsPassed: string[]; }>{
+    address: string,
+    rule: { addresses: [string]; discountPercentage: number; nftsCount: number;},
+    reedemedNFTs: any,
+    network : "mainnet" | "testnet",
+){
     let total_amount = 0;
     let NFTsPassed : string[] = [];
     for (let i = 0 ; i < rule.addresses.length; i++){
         let contract_address = rule.addresses[i];
-        let nftAmount = await get_address_amount(contract_address, address, network);
+        let nftAmount = await get_address_amount(contract_address, address,network);
         total_amount += nftAmount;
         if (!reedemedNFTs.includes(contract_address) && nftAmount!=0){
             NFTsPassed.push(contract_address);
@@ -66,31 +54,29 @@ async function nonGatedPassesRules(
 /**
  * Checks if the address passes any of the rules in the ruleset sorted by discount percentage
  * @param {string} address 
- * @param {{redeemedNFTs: [*], gated : boolean , rules: [{addresses : [string], discountPercentage: number, nftsCount : number , description:string, _id : string}]}} ruleset 
+ * @param {{redeemedNFTs: [*], gated : boolean , rules: [{addresses : [string], discountPercentage: number, nftsCount : number}]}} ruleset 
  * @returns { Promise<{discountPercentage : number, NFTsPassed : [string]}> } the discount percentage and the NFTs that passed the rules
  */
-async function getMaxDiscount(address: any, ruleset: { rules: any[]; redeemedNFTs: any; }, apiKey : string, network : "polygon-testnet" | "polygon-mainnet" | "binance-testnet" | "binance-mainnet"): Promise<{ discountPercentage: number; NFTsPassed: string[]; }>{
-    if (!isMoralisInitialized){
-        await Moralis.start({
-            apiKey: apiKey,
-        });
-        isMoralisInitialized = true;
-    }
-    const max_discount = {
+async function getMaxDiscount(address: string, ruleset: { redeemedNFTs: [any]; gated: boolean; rules: [{ addresses: [string]; discountPercentage: number; nftsCount: number; }]; }, network : "mainnet" | "testnet" = "testnet"): Promise<{ discountPercentage: number; NFTsPassed: string[]; }>{
+    let max_discount : {
+        discountPercentage : number,
+        NFTsPassed : string[]
+    } = {
         discountPercentage :0,
         NFTsPassed : []
     };
-    ruleset.rules.sort((a: { discountPercentage: number; },b: { discountPercentage: number; }) => {
+    ruleset.rules.sort((a,b) => {
         return b.discountPercentage - a.discountPercentage;
     });
     for (let i = 0 ; i < ruleset.rules.length; i++){
         let rule = ruleset.rules[i];
         const result = await nonGatedPassesRules(address, rule, ruleset.redeemedNFTs, network);
         if (result.passes){
-            return {
+            max_discount = {
                 discountPercentage: rule.discountPercentage,
                 NFTsPassed : result.NFTsPassed
             };
+            break;
         }
     }
     return max_discount;
@@ -98,25 +84,21 @@ async function getMaxDiscount(address: any, ruleset: { rules: any[]; redeemedNFT
 /**
  * 
  * @param {string} address 
- * @param {{redeemedNFTs: [*], gated : boolean , rules: [{addresses : [string], discountPercentage: number, nftsCount : number , description:string, _id : string}]}} ruleset 
+ * @param {{redeemedNFTs: [*], gated : boolean , rules: [{addresses : [string], discountPercentage: number, nftsCount : number}]}} ruleset 
  * @returns {Promise<boolean>} true if the address passes any of the rules in the ruleset 
 */
-async function gatedPassesRules(address: any,
-    ruleset: { rules: string | any[]; }, apiKey : string, network : "polygon-testnet" | "polygon-mainnet" | "binance-testnet" | "binance-mainnet"): Promise<boolean> {
-    if (!isMoralisInitialized){
-        await Moralis.start({
-            apiKey: apiKey,
-        });
-        isMoralisInitialized = true;
-    }
-
+const gatedPassesRules = async(
+    address: string,
+    ruleset: { redeemedNFTs: [any]; gated: boolean; rules: [{ addresses: [string]; discountPercentage: number; nftsCount: number; }]; },
+    network : "mainnet" | "testnet" = "testnet"
+): Promise<boolean> => {
     let passes = false;
-    for (let i = 0; i < ruleset.rules.length; i++) {
+    for (let i = 0 ; i < ruleset.rules.length; i++){
         let rule = ruleset.rules[i];
         let total_amount = 0;
-        for (let j = 0; j < rule.addresses.length; j++) {
+        for (let j = 0 ; j < rule.addresses.length; j++){
             let contract_address = rule.addresses[j];
-            let amount = await get_address_amount(contract_address, address, network);
+            let amount = await get_address_amount(contract_address, address,network);
             total_amount += amount;
         }
         passes = total_amount >= rule.nftsCount;
